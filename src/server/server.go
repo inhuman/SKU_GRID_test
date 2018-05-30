85cbddb227260a58fc93488297648d7379fd1afc
Ivan <msgexec@gmail.com>
2018-05-30 10:18:32 +0300

    some refactor

--- src/server/server.go
+++ src/server/server.go
@@ -2,21 +2,13 @@ package server
 
 import (
 	"github.com/gorilla/mux"
-	"log"
 	"strconv"
 	"net/http"
-	"github.com/gorilla/handlers"
+	"fmt"
 )
 
 func Start(router *mux.Router, port *int) {
-
-	log.Println("Starting sku grid server on port :" + strconv.Itoa(*port))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), handlers.CORS(getCORS())(router)))
+	fmt.Println("Starting sku grid server on port :" + strconv.Itoa(*port))
+	http.ListenAndServe(":"+strconv.Itoa(*port), router)
 }
 
-func getCORS() (handlers.CORSOption) {
-
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
-	return methodsOk
-}
